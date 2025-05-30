f49568176bab36170808f6ea3ac99df3ad0a8b5a
Santiago Martinez <smyardev2@gmail.com>
2025-05-30 14:36:38 -0500

    DEVOPS-2: Implementacion del core de la aplicacion

---
+++ todor/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from .config import Config
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+
+    # Registrar Blueprints
+    from . import todo
+    app.register_blueprint(todo.bp)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    with app.app_context():
+        db.create_all()
+
+    return app
